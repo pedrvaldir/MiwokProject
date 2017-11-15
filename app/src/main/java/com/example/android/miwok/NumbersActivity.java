ae6f0ff1de89a5e095ca8f56bb484f85fee775cc
Valdir.Pedroso <pedrvaldir@gmail.com>
2017-11-15 13:30:49 -0200

    comentarioloops retirado

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,11 +2,8 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TextView;
+
 
 import java.util.ArrayList;
 
@@ -18,7 +15,7 @@ public class NumbersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_numbers);
 
         //CRIAR A LISTA DE PALAVRAS
-        ArrayList<Word> words = new ArrayList<Word>();
+        ArrayList<Word> words = new ArrayList<>();
 
         //words.add("one");
         words.add(new Word("One", "Lutti"));
@@ -27,34 +24,18 @@ public class NumbersActivity extends AppCompatActivity {
         words.add(new Word("four", "oyyisa"));
         words.add(new Word("five", "massokka"));
         words.add(new Word("six", "temmokka"));
-        words.add(new Word("seven","kenekaku"));
-        words.add(new Word("eight","kawinta"));
-        words.add(new Word("nine","wo'e"));
-        words.add(new Word("ten","na'aacha"));
+        words.add(new Word("seven", "kenekaku"));
+        words.add(new Word("eight", "kawinta"));
+        words.add(new Word("nine", "wo'e"));
+        words.add(new Word("ten", "na'aacha"));
 
         //Criando lista de arrayAdapter
-        WordAdapter adapter  = new WordAdapter(this, words);
+        WordAdapter adapter = new WordAdapter(this, words);
 
         ListView listView = (ListView) findViewById(R.id.list);
 
+        assert listView != null;
         listView.setAdapter(adapter);
 
-
-        //  ListView listView = (ListView) findViewById(R.id.list);
-
-    /*
-    introdu
-        for (int index = 0; index <words.size();index++){
-            //Criado view pai(rootView) que recebera a view filha WordView
-            LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
-            TextView wordView = new TextView(this);
-
-            //adicionar a palavar do indice para um textview
-            wordView.setText(words.get(index));
-
-            //Adicionar a filha(WordView ao pai (rootView)
-            rootView.addView(wordView);
-        }
-   */
-   }
+    }
 }
